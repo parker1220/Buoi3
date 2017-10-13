2bab61629b7d517d88a422b3792dd3301805bb3a
parker1220 <nguyenphucluc1220@gmail.com>
2017-10-13 20:28:48 +0700

    Oẳn tù tì

---
+++ LucNguyenPhuc_Buoi3/KeoBuaBao/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace KeoBuaBao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+            server.Bind(ipep);
+
+            Console.WriteLine("Waiting for a client...");
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+
+            byte[] data = new byte[4];
+            server.ReceiveFrom(data, ref Remote);
+            int clientChoosen = BitConverter.ToInt32(data, 0);
+
+            Random rand = new Random();
+            int serverChoosen = rand.Next(0, 2);
+
+            if (clientChoosen == serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Hoa");
+                server.SendTo(send, Remote);
+            }
+            if (clientChoosen == 0 && serverChoosen == 1)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, Remote);
+            }
+            if (clientChoosen == 1 && serverChoosen == 2)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, Remote);
+            }
+            if (clientChoosen == 2 && serverChoosen == 0)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, Remote);
+            }
+            if (clientChoosen == 1 && serverChoosen == 0)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thắng");
+                server.SendTo(send, Remote);
+            }
+            if (clientChoosen == 2 && serverChoosen == 1)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thắng");
+                server.SendTo(send, Remote);
+            }
+            if (clientChoosen == 0 && serverChoosen == 2)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thắng");
+                server.SendTo(send, Remote);
+            }
+
+        }
+    }
+}
+                    
---
+++ LucNguyenPhuc_Buoi3/Server/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Server : Form
+    {
+        Socket server;
+        Socket client;
+        byte[] data;
+
+        private bool btn0clicked = false;
+        private bool btn1clicked = false;
+        private bool btn2clicked = false;
+        int num;
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            server.Bind(ipep);
+            server.Listen(10);
+            client = server.Accept();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Kéo";
+            btn0clicked = true;
+            btn1clicked = false;
+            btn2clicked = false;
+            send();
+            receive();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Búa";
+            btn0clicked = false;
+            btn1clicked = false;
+            btn2clicked = true;
+            send();
+            receive();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bao";
+            btn0clicked = false;
+            btn1clicked = true;
+            btn2clicked = false;
+            send();
+            receive();
+        }
+        public void send()
+        {
+            if (btn0clicked == true)
+            {
+                num = 0;
+                data = Encoding.ASCII.GetBytes(num.ToString());
+                client.Send(data);
+            }
+            if (btn1clicked == true)
+            {
+                num = 1;
+                data = Encoding.ASCII.GetBytes(num.ToString());
+                client.Send(data);
+            }
+            if (btn2clicked == true)
+            {
+                num = 2;
+                data = Encoding.ASCII.GetBytes(num.ToString());
+                client.Send(data);
+            }
+        }
+        public void receive()
+        {
+            data = new byte[1024];
+
+            if (data != null)
+            {
+                client.Receive(data);
+                int data1 = Convert.ToInt32(Encoding.ASCII.GetString(data));
+                if (btn0clicked)
+                {
+                    if (data1 == 0)
+                        textBox2.Text = "Hòa";
+                    if (data1 == 1)
+                        textBox2.Text = "Thắng";
+                    if (data1 == 2)
+                        textBox2.Text = "Thua";
+
+                }
+                if (btn1clicked)
+                {
+                    if (data1 == 0)
+                        textBox2.Text = "Thua";
+                    if (data1 == 1)
+                        textBox2.Text = "Hòa";
+                    if (data1 == 2)
+                        textBox2.Text = "Thắng";
+                }
+                if (btn2clicked)
+                {
+                    if (data1 == 0)
+                        textBox2.Text = "Thắng";
+                    if (data1 == 1)
+                        textBox2.Text = "Thua";
+                    if (data1 == 2)
+                        textBox2.Text = "Hòa";
+                }
+            }
+
+        }
+    }
+}
